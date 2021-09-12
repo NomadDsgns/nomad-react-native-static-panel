71dc7502cb958759689da6827cbd9c21073c3539
Daniel Hawkins <90428134+NomadDsgns@users.noreply.github.com>
2021-09-11 22:02:14 -0500

    updated panel component

--- NomadStaticPanel.js
+++ NomadStaticPanel.js
@@ -1,1 +1,47 @@
+import React from 'react';
+import { Text, View } from 'react-native';
 
+export default function({ title, children }) {
+
+    return (
+        <View>
+            { title &&
+            <View>
+                <View style={styles.titleContainer} >
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </View>
+            }
+            <View style={styles.panelContainer} >
+                {children}
+            </View>
+        </View>               
+    );
+};
+
+let styles = {
+    titleContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        padding: 10,
+        alignItems: "center",
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+    },
+    title: {
+        fontWeight: '700'
+    },
+    panelContainer: {
+        marginLeft: 5,
+        marginRight: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+    }
+};
