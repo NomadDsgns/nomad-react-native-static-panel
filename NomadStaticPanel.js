13b5322ad5fb0f8970b553921d26b100719325ab
Daniel Hawkins <90428134+NomadDsgns@users.noreply.github.com>
2021-09-11 22:23:52 -0500

    Update NomadStaticPanel.js

--- NomadStaticPanel.js
+++ NomadStaticPanel.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function({ title, children }) {
-
     return (
         <View>
             { title &&
